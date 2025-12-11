7c6def2c4f530a40967c65830e253a72f90affab
Vinit Kumar <tyagivinit96@gmail.com>
2025-12-12 00:56:42 +0530

    Uncomment Azure Resource Group and Storage Account modules

--- Parent/main.tf
+++ Parent/main.tf
@@ -1,19 +1,19 @@
-# # Azure Resource Group
+ # Azure Resource Group
 
-# module "rg_new" {
-#   source   = "../child/resource_group"
-#   rg_new   = "vinit-rg"
-#   location = "centralindia"
-# }
+ module "rg_new" {
+   source   = "../child/resource_group"
+   rg_new   = "vinit-rg"
+   location = "centralindia"
+ }
 
-# # Azure Storage Account
+ # Azure Storage Account
 
-# module "new_stg" {
-#   depends_on = [ module.rg_new ]
-#   source   = "../child/storage_account"
-#   new_stg  = "storageaccountvinit"
-#   rg_new   = "vinit-rg"
-#   location = "centralindia"
-# account_tier = "Standard"
-# account_replication_type = "GRS"
-# }
+ module "new_stg" {
+   depends_on = [ module.rg_new ]
+   source   = "../child/storage_account"
+   new_stg  = "storageaccountvinit"
+   rg_new   = "vinit-rg"
+   location = "centralindia"
+ account_tier = "Standard"
+ account_replication_type = "GRS"
+ }
